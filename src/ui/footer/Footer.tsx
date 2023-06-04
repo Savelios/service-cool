f0b53c8fa6afa304be7de45eea14fd8274b95809
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-06-04 20:20:37 +0300

    add

--- src/ui/footer/Footer.tsx
+++ src/ui/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './footer.css'
+import logo from "../../assets/logo.png";
 
 export function Footer() {
     return (
         <React.Fragment>
             <div className="footer">
-                <h1 className="footer-logo">HYDRO-MINIG</h1>
+                <h1 className="footer-text-logo">HM</h1>
+                <img src={logo} className="footer-img-logo" alt="logo" />
                 <h1 className="contacts-text">п. Санамер ул. Микояна 162</h1>
                 <button className="order-btn">ЗАКАЗАТЬ</button>
             </div>
--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './header.css'
+import logo from "../../assets/logo.png";
 
 export function Header() {
     return (
         <React.Fragment>
             <div className="header">
-                <h1 className="header-logo">HYDRO-MINIG</h1>
+                <h1 className="header-text-logo">HYDRO MINIG</h1>
+                <img src={logo} className="header-img-logo" alt="logo" />
                 <label className="about-us-label" htmlFor="">О НАС</label>
                 <label className="gallery-label" htmlFor="">ГАЛЕРЕЯ</label>
                 <label className="contacts-label" htmlFor="">КОНТАКТЫ</label>
