95c98b8ab18e60283cc70e5ff8c0a3d06427c42c
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-09-22 23:14:17 +0300

    add

--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -1,24 +1,72 @@
-import React from "react";
-import './header.css'
-import logo from "../../assets/logo.png";
+import React, { useState, useEffect, useRef } from "react";
+import "./header.css";
 import { useNavigate } from "react-router-dom";
 
 export function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <React.Fragment>
-            <div className="header">
-                <h1 className="header-text-logo" onClick={() => navigate('/main')}
-                >HYDRO MINIG</h1>
-                {/* <img src={logo} className="header-img-logo" alt="logo" /> */}
-                <label className="about-us-label" htmlFor="" onClick={() => navigate('/about')}
-                >О НАС</label>
-                <label className="gallery-label" htmlFor="" onClick={() => navigate('/gallery')}
-                >ГАЛЕРЕЯ</label>
-                <label className="contacts-label" htmlFor="" onClick={() => navigate('/contacts')}
-                >КОНТАКТЫ</label>
-            </div>
-        </React.Fragment>
-    );
+  const [underlinedLabel, setUnderlinedLabel] = useState("");
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const handleClick = (route: string) => {
+    navigate(route);
+  };
+
+  const handleLabelClick = (label: string) => {
+    setUnderlinedLabel(label);
+    navigate("/" + label);
+  };
+
+  const handleDocumentClick = (e: MouseEvent) => {
+    // Проверяем, был ли клик вне меню
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      setUnderlinedLabel(""); // Сбрасываем подчеркивание
+    }
+  };
+
+  useEffect(() => {
+    // Добавляем обработчик события для клика на документе
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      // Удаляем обработчик события при размонтировании компонента
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div className="header">
+        <h1 className="header-text-logo" onClick={() => handleClick("/main")}>
+          HYDRO MINIG
+        </h1>
+        <div ref={menuRef} className="menu-container">
+          <label
+            className={`menu-label ${
+              underlinedLabel === "about" ? "underlined" : ""
+            }`}
+            onClick={() => handleLabelClick("about")}
+          >
+            О НАС
+          </label>
+          <label
+            className={`menu-label ${
+              underlinedLabel === "gallery" ? "underlined" : ""
+            }`}
+            onClick={() => handleLabelClick("gallery")}
+          >
+            ГАЛЕРЕЯ
+          </label>
+          <label
+            className={`menu-label ${
+              underlinedLabel === "contacts" ? "underlined" : ""
+            }`}
+            onClick={() => handleLabelClick("contacts")}
+          >
+            КОНТАКТЫ
+          </label>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
