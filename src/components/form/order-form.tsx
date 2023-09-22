72083bf4aaf891ad871e48f338e022f75388a65c
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-09-22 20:01:41 +0300

    add

--- src/components/form/order-form.tsx
+++ src/components/form/order-form.tsx
@@ -1,50 +1,99 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import "./order-form.css";
-import closeIcon from "../../assets/close-form-btn.png"
+import closeIcon from "../../assets/close-form-btn.png";
+import axios from "axios";
 
 type ModalProps = {
-    onCloseModal: () => void;
+  onCloseModal: () => void;
 };
 
 const OrderForm: React.FC<ModalProps> = ({ onCloseModal }) => {
-    return (
-        <>
-            <div className="main-order-container"  >
-                <div className="order-container">
-                    <img className="close-form-button" src={closeIcon} alt="" onClick={onCloseModal} /> 
-                    <h2 className="order-title">Оставьте вашу заявку и мы с Вами обязательно свяжемся!</h2>
-                    <div className="input-items-main-container">
-                        <div className="input-items-container">
-                            <label className="name-prefix-label" htmlFor="uname">
-                                ПРЕДСТАВЬТЕСЬ ПОЖАЛУЙСТА
-                            </label>
-                            <input
-                                type="text"
-                                id="uname"
-                                placeholder="ФИО"
-                                className="name-input"
-                                required
-                                minLength={2}
-                                maxLength={18}
-                            ></input>
-                            <label className="phone-prefix-label" htmlFor="phone">
-                                ТЕЛЕФОН
-                            </label>
-                            <input
-                                type="text"
-                                id="phone"
-                                placeholder="+7 ( - - - ) - - -  - -  - -"
-                                className="phone-input"
-                                required
-                                minLength={11}
-                                maxLength={11}
-                            ></input>
-                            <button className="form-order-button">Оставить заявку</button>
-                        </div>
-                    </div>
-                </div>
+  const [username, setUsername] = useState("");
+  const [phonenumber, setPhonenumber] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // const data = {username: username, phonenumber: phonenumber};
+
+    // axios.post("http://localhost:3001/register", data)
+
+    try {
+      const response = await axios.post("http://localhost:3001/send-message", {
+        username,
+        phonenumber,
+      });
+
+      setResponseMessage(response.data.message);
+    } catch (error) {
+      console.error(error);
+      setResponseMessage("Произошла ошибка при отправке сообщения.");
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <div className="main-order-container">
+        <div className="order-container">
+          <form onSubmit={handleSubmit}>
+            <img
+              className="close-form-button"
+              src={closeIcon}
+              alt=""
+              onClick={onCloseModal}
+            />
+            <h2 className="order-title">
+              Оставьте вашу заявку и мы с Вами обязательно свяжемся!
+            </h2>
+            <div className="input-items-main-container">
+              <div className="input-items-container">
+                <label className="name-prefix-label" htmlFor="uname">
+                  ПРЕДСТАВЬТЕСЬ ПОЖАЛУЙСТА
+                </label>
+                <input
+                  type="text"
+                  className="name-input"
+                  id="uname"
+                  placeholder="ФИО"
+                  required
+                  minLength={2}
+                  maxLength={36}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <label className="phone-prefix-label" htmlFor="phone">
+                  ТЕЛЕФОН
+                </label>
+                <input
+                  type="text"
+                  className="phone-input"
+                  id="phone"
+                  placeholder="+7 ( - - - ) - - -  - -  - -"
+                  required
+                  minLength={10}
+                  maxLength={18}
+                  value={phonenumber}
+                  onChange={(e) => setPhonenumber(e.target.value)}
+                />
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="form-order-button"
+                >
+                  Оставить заявку
+                </button>
+              </div>
             </div>
-        </>
-    );
-}
+          </form>
+          {responseMessage && <p>{responseMessage}</p>}
+        </div>
+      </div>
+    </>
+  );
+};
 export default OrderForm;
---
+++ src/service/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 1,
+});
+
+app.use(bodyParser.json());
+
+app.post("/send-message", limiter, async (req, res) => {
+  try {
+    const { username, phonenumber } = req.body;
+
+    const text = `Имя: ${username}\nТелефон: ${phonenumber}`;
+
+    const TelegramBotToken = "6624470838:AAH25_vATb_RkmxpHq6ZAyqE25snmwprrwo";
+    const TelegramChatId = "-4024471935";
+
+    await axios.post(
+      `https://api.telegram.org/bot${TelegramBotToken}/sendMessage`,
+      {
+        chat_id: TelegramChatId,
+        text,
+      }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Сообщение успешно отправлено в Telegram!" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Произошла ошибка при отправке сообщения." });
+  }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
